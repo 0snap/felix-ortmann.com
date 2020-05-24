09e9679928106cceddf96365f160990041b906e6
Felix Ortmann <flx.ortmann@gmail.com>
2020-05-24 13:23:15 +0200

    Load some german content from yaml

--- src/pages/index.de.js
+++ src/pages/index.de.js
@@ -7,22 +7,22 @@ import SEO from "../components/seo"
 import { graphql } from "gatsby"
 
 const IndexPage = ({ data }) => (
-  <Page>
+  <Page lang="de">
     <SEO title="Felix Ortmann" />
     <Cover />
     <section className="section is-medium">
       <h4 className="title is-4 has-text-centered">Profile</h4>
       <div className="container">
-        <p>{data.dataYaml.content.de.profile}</p>
+        <p>{data.dataYaml.aboutme.de.profile}</p>
       </div>
     </section>
   </Page>
 )
 
 export const query = graphql`
-  query AboutMeQuery {
+  query ProfileQuery {
     dataYaml {
-      content {
+      aboutme {
         de {
           profile
         }
