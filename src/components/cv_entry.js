4cdcaffa3a4980cb259b18b6c3643debf2e4a684
Felix Ortmann <flx.ortmann@gmail.com>
2022-01-06 17:24:39 +0100

    adjust columns

--- src/components/cv_entry.js
+++ src/components/cv_entry.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const CvEntry = ({ entry }) => (
   <div className="columns mb-6">
-    <div className="column is-two-fifths">{entry.image}</div>
-    <div className="column is-three-fifths content box">
+    <div className="column is-one-fifths">{entry.image}</div>
+    <div className="column is-four-fifths content box">
       <p className="is-size-4 my-3">
         <a href={entry.link} target="_blank" rel="noopener noreferrer">
           {entry.company}
