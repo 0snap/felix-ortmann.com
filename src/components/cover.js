9930821e74add77a7711f1c2331877485cf9b4b7
Felix Ortmann <flx.ortmann@gmail.com>
2022-01-06 17:24:12 +0100

    new hero cover

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,12 +1,29 @@
 import React from "react"
 
 const Cover = () => (
-  <section className="hero is-primary is-fullheight">
+  <section className="hero is-fullheight background-hero">
     <div className="hero-body">
-      <div className="container">
-        <h1 className="title is-size-1">Felix Ortmann</h1>
-        <h2 className="subtitle">I like to code, I love to think.</h2>
+      <div className="container has-text-centered">
+        <h1 className="intro is-size-3-mobile	is-size-1-desktop">
+          Beautiful clouds, huh? <br />
+          I'm a cloud engineer.
+        </h1>
       </div>
+      <a className="arrow-down" href="#about_me">
+        <ul>
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+        </ul>
+      </a>
     </div>
   </section>
 )
