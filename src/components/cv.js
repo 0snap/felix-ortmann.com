f2b679528fb3cb85546f3cba3e074199ee961f13
Felix Ortmann <flx.ortmann@gmail.com>
2022-01-08 18:21:32 +0100

    move titles to components

--- src/components/cv.js
+++ src/components/cv.js
@@ -4,6 +4,9 @@ import CvEntry from "./cv_entry"
 
 const Cv = ({ journey }) => (
   <div className="container">
+    <p className="is-size-3 has-text-centered has-title-line mb-6">
+      My journey
+    </p>
     {journey &&
       journey.map((cvEntry, idx) => <CvEntry key={idx} entry={cvEntry} />)}
   </div>
--- src/components/profile.js
+++ src/components/profile.js
@@ -4,6 +4,9 @@ import ProfileEntry from "./profile_entry"
 const AboutMe = ({ profile }) => {
   return (
     <div className="container">
+      <p className="is-size-3 has-text-centered has-title-line my-6">
+        About me
+      </p>
       {profile &&
         profile.map((entry, idx) => (
           <ProfileEntry key={idx} idx={idx} entry={entry} />
