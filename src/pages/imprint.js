ae3f364c137e0487ba048304630f01d2dd651031
Felix Ortmann <flx.ortmann@gmail.com>
2022-12-18 18:52:24 +0100

    imprint + privpolicy w/ dedicated yaml source

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -4,22 +4,23 @@ import Page from "../components/page"
 import Seo from "../components/seo"
 
 import contact from "../../content/contact.yaml"
+import imprint from "../../content/imprint.yaml"
 
 const ImprintPage = () => {
   return (
     <Page>
-      <Seo title="Legal Disclosure" />
+      <Seo title={imprint.headline} />
       <section className="section is-medium bg-one">
         <div className="container">
           <div className="content">
             <header>
-              <h1 className="title is-2">Legal Disclosure</h1>
+              <h1 className="title is-2">{imprint.headline}</h1>
             </header>
           </div>
 
           <div className="content">
             <h3 className="title">
-              Information in accordance with Section 5 TMG
+              {imprint.contact_headline}
             </h3>
             <p>
               {contact.name}
@@ -38,47 +39,12 @@ const ImprintPage = () => {
             </p>
           </div>
 
-          <div className="content">
-            <h3 className="title">Accountability for Content</h3>
-            <p>
-              The contents of these web pages have been created with the utmost
-              care. Nonetheless, we cannot guarantee the contents' correctness,
-              completeness, accuracy, or topicality. According to statutory
-              provisions as per § 7 of the Telemedia Act (TMG), we are
-              responsible for our own content on these web pages. Please note
-              that we are not obliged to monitor the transmitted or saved
-              information of third parties, or investigate circumstances
-              pointing to illegal activity as per §§ 8 to 10 of the Telemedia
-              Act (TMG). Our obligations to block or remove the use of
-              information under generally applicable law remain unaffected by
-              this.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Accountability for Links</h3>
-            <p>
-              Our web pages contain links to third party websites (external
-              links), for which's contents we are not responsible.
-              Responsibility for the content of external links lies solely with
-              the operators of the linked pages. No violations or fraudulent
-              activity were evident to us at the time of linking. We will remove
-              links immediately from our web pages should any legal infringement
-              become known to us.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Copyright</h3>
-            <p>
-              Our web pages and their contents are created by the website owner
-              and are subject to German copyright law. Every form of utilizing,
-              reproducing, or processing works subject to copyright protection
-              on our web pages requires the prior consent of the respective
-              owner of the rights, unless expressly permitted by law. Individual
-              reproductions of a work are only allowed for private use. The
-              materials from these pages are copyrighted and any unauthorized
-              use may violate copyright laws.
-            </p>
-          </div>
+          {imprint.paragraphs.map((p, idx) => {
+            return (<div key={idx} className="content">
+              <h3 className="title">{p.title}</h3>
+              <p>{p.text}</p>
+            </div>)
+          })}
         </div>
       </section>
     </Page>
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -4,37 +4,23 @@ import Page from "../components/page"
 import Seo from "../components/seo"
 
 import contact from "../../content/contact.yaml"
+import pp from "../../content/privacy-policy.yaml"
 
 const PrivacyPolicyPage = () => {
   return (
     <Page>
-      <Seo title="Privacy Policy" />
+      <Seo title={pp.headline}/>
       <section className="section is-medium bg-one">
         <div className="container">
           <div className="content">
             <header>
-              <h1 className="title is-2">Privacy Policy</h1>
+              <h1 className="title is-2">{pp.headline}</h1>
             </header>
           </div>
           <div className="content">
-            <p>
-              In this privacy statement, we inform you about the personal data
-              we collect from you while using our website, how we use your data,
-              and for what purpose we store the recorded information.
-            </p>
-            <p>
-              We keep this privacy policy under regular review and place any
-              updates on this website. This privacy policy was last updated on
-              March 25th, 2021.
-            </p>
-            <p>
-              Please find the definitions of the terms used (e.g., "personal
-              data" or "processing") in{" "}
-              <a href="https://gdpr-info.eu/art-4-gdpr/">
-                Article 4 EU GDPR "Definitions"
-              </a>
-              .
-            </p>
+            {pp.disclaimer.map((d, idx) => {
+              return <p key={idx} dangerouslySetInnerHTML={{ __html: d }}/>
+            })}
           </div>
           <div className="content">
             <h3 className="title">Website Operator</h3>
@@ -54,82 +40,13 @@ const PrivacyPolicyPage = () => {
               {contact.ustid}
             </p>
           </div>
-          <div className="content">
-            <h3 className="title">General Data</h3>
-            <p>
-              We use <a href="https://pages.github.com/">GitHub Pages</a> to
-              host our website. By visiting our website, GitHub collects
-              personal information (e.g., in the form of server log files),
-              including, but not limited to, your IP address, the date and time
-              of your request, your web browser, the referring site, language
-              preferences, and additional websites requested. GitHub needs this
-              information to ensure functionality and security of the service.
-              GitHub is certified to the{" "}
-              <a href="https://docs.github.com/en/github/site-policy/global-privacy-practices#privacy-shield-framework">
-                Privacy Shield Framework
-              </a>{" "}
-              . Please consult{" "}
-              <a href="https://docs.github.com/en/github/site-policy/github-privacy-statement">
-                GitHub's Privacy Notice
-              </a>{" "}
-              for details on their privacy statement.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Personal Data</h3>
-            <p>
-              We, the website operator, do not collect personal data about our
-              users directly. You can visit out website without disclosing
-              personal information to us. Personal data relates to all
-              informations that can be used for personal identification, such as
-              your contact details, your E-Mail address, or telephone number.
-            </p>
-            <p>
-              If you contact me, the website operator, via E-Mail, I will store
-              and use your E-Mail address only to answer you inquiry. All
-              personal information that was collected in the process of
-              answering your inquiry will be deleted once your inquiry has been
-              resolved.
-            </p>
-            <p>
-              We will not use your data for other purposes nor transfer it to a
-              third party without having obtained your prior consent.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Data Security</h3>
-            <p>
-              We deploy technical measures to ensure the security of your data
-              and to prevent unauthorized access and changes or loss of your
-              data. Your connection to the server hosting this website is
-              encrypted via TLS. You can verify the encrypted connection by
-              checking the address bar of your browser, which should show a
-              green lock symbol. The measures reflect the state of the art and
-              are reviewed and updated at regular intervals.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Your Rights</h3>
-            <p>
-              You are at any time entitled to request information on what
-              personal data we store about you, request deletion (entirely or
-              partially), rectification, or blocking of your personal data. You
-              can at any time object to further use of your personal data. If
-              applicable, you can use your right to data portability. Please
-              address your concern using my contact details from the top of this
-              page. Should you consider the processing of your personal data as
-              unlawful, you can directly submit your complaint with the
-              responsible supervisory authority.
-            </p>
-          </div>
-          <div className="content">
-            <h3 className="title">Data Retention</h3>
-            Unless legal requirements oblige us to retain your data for a longer
-            period (e.g., storage periods according to trade or tax
-            regulations), we retain your data only for as long as it is
-            necessary to perform the service that you have requested or for
-            which you have granted your permission.
-          </div>
+          {pp.paragraphs.map((p, idx) => {
+            return (
+              <div key={idx} className="content">
+                <h3 className="title">{p.title}</h3>
+                <p dangerouslySetInnerHTML={{ __html: p.text }}/>
+              </div>);
+          })}
         </div>
       </section>
     </Page>
