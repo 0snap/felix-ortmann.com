d6ff6b062ae7bd54c9295acd0b855d6a3adf82b9
Felix Ortmann <flx.ortmann@gmail.com>
2022-12-19 17:08:37 +0100

    lazy-load hero image

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -1,20 +1,42 @@
 import * as React from "react"
 
+import { StaticImage } from "gatsby-plugin-image"
+
 const Cover = () => (
-  <section className="hero is-fullheight background-hero">
-    <div className="hero-body">
-      <div className="header-box has-text-centered is-size-3-touch	is-size-1-desktop">
-        <h1 className="header-text">
-          Beautiful clouds, huh? <br />
-          I'm a cloud engineer.
-        </h1>
+  <section className="hero is-fullheight" style={{ display: "grid" }}>
+      <StaticImage
+        style={{ gridArea: "1/1" }}
+        alt="Background image clouds"
+        src="../../images/clouds.jpg"
+        formats={["auto"]}
+        placeholder="blurred"
+        objectFit="cover"
+        quality={100}
+        transformOptions={{
+          fit: "cover",
+          cropFocus: "attention",
+        }}
+      />
+      <div className="hero-body hero-content" style={{
+          // Stack content and Gatsby image by using the same grid area for both
+          gridArea: "1/1",
+          position: "relative",
+          // Center the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}>
+        <div className="header-box has-text-centered is-size-3-touch	is-size-1-desktop">
+          <h1 className="header-text">
+            Beautiful clouds, huh? <br />
+            I'm a cloud engineer.
+          </h1>
+        </div>
       </div>
-    </div>
-    <div className="arrow-down">
-      <a aria-label="Read more about Felix Ortmann" href="#about_me">
-        <span></span>
-      </a>
-    </div>
+      <div className="arrow-down">
+        <a aria-label="Read more about Felix Ortmann" href="#about_me">
+          <span></span>
+        </a>
+      </div>
   </section>
 )
 
