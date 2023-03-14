b2fc85a92fea65b3ce2ab8f95353e95ffea91e81
Felix Ortmann <flx.ortmann@gmail.com>
2023-03-14 21:26:17 +0100

    Update 404 page styles & back link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 
+import {Link} from 'gatsby';
+import {TfiBackLeft} from 'react-icons/tfi';
+
 import Seo from '../components/seo';
 import Page from '../components/page';
 
 const NotFoundPage = () => (
   <Page>
-    <section className="section is-medium">
+    <section className="section bg">
       <div className="container">
-        <div className="content has-text-centered">
-          <h3 className="title">Not found.</h3>
+        <div className="content has-text-centered is-size-4 mt-6 pt-6">
+          <p>Page not found.</p>
+          <Link to="/">
+            Go back<TfiBackLeft/>
+          </Link>
         </div>
       </div>
     </section>
