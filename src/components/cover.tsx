7fd81647b5d2f5dbeb4e5fdccda866dcfb81b46e
Felix Ortmann <flx.ortmann@gmail.com>
2023-04-17 20:57:22 +0200

    fix label

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -36,7 +36,7 @@ const Cover = () => (
       </div>
     </div>
     <div className="arrow-down">
-      <a aria-label="Read more about Felix Ortmann" href="#landing">
+      <a aria-label="Landing page" href="#landing">
         <span></span>
       </a>
     </div>
