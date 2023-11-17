3b5efaeb200c252330903a7e5a8af3363fab8541
Felix Ortmann <flx.ortmann@gmail.com>
2023-11-17 17:01:16 +0100

    Fixate css rocket class

--- src/components/rocket.tsx
+++ src/components/rocket.tsx
@@ -3,12 +3,11 @@ import {HiOutlineRocketLaunch} from 'react-icons/hi2';
 
 type RocketProps = {
     size: string;
-    className: string;
 }
 
 // see https://github.com/react-icons/react-icons/issues/251
-const Rocket = ({size, className}: RocketProps) =>
-  (<div className={className}>
+const Rocket = ({size}: RocketProps) =>
+  (<div className='rocket'>
     <svg width="0" height="0">
       <linearGradient id="rocket-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
         <stop stopColor="#ee4a22" offset="0%" />
