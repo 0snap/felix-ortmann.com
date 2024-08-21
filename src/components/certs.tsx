fc9643f3232db6e0bdaef192e32460750be39516
Felix Ortmann <flx.ortmann@gmail.com>
2024-08-21 13:05:53 +0200

    Add Scaleway Network Associate certificate

--- src/components/certs.tsx
+++ src/components/certs.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import ScwBadge from '../../images/scw_cert_badge.svg';
+import ScwBadgeFoundations from '../../images/scw_cert_badge.svg';
+import ScwBadgeNetworkAssociate from '../../images/scw_cert_badge_network.svg';
 
 
 const Certs = () => {
   return <>
     <section className='section teaser mb-6' id='services'>
-      <div className='columns is-mobile is-vcentered' style={{maxWidth: '1280px', margin: 'auto'}}>
-        <div className='column is-hidden-mobile'>
+      <div className='columns is-mobile is-vcentered is-centered' style={{maxWidth: '1280px', margin: 'auto'}}>
+        <div className='column is-hidden-mobile is-three-fifths '>
           <h3 className='is-size-3'>
           Certified by <a href="https://scaleway.com" target="_blank" rel="noopener noreferrer">
             Scaleway
@@ -14,9 +15,14 @@ const Certs = () => {
           , Europe's empowering cloud provider
           </h3>
         </div>
-        <div className='column'>
+        <div className='column is-two-fifths-mobile'>
           <a href="https://scaleway.360learning.com/api/certification/5330443733204762" target="_blank" rel="noopener noreferrer">
-            <ScwBadge className='cert-badge' />
+            <ScwBadgeFoundations className='cert-badge' />
+          </a>
+        </div>
+        <div className='column is-two-fifths-mobile'>
+          <a href="https://scaleway.360learning.com/api/certification/3912943105992750" target="_blank" rel="noopener noreferrer">
+            <ScwBadgeNetworkAssociate className='cert-badge' />
           </a>
         </div>
       </div>
